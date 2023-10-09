9b4353b4246a3b6be5081f069ad154d0e8d8d9a4
Anh Thi Lan Nguyen <lananhnt@outlook.com.vn>
2023-10-10 02:39:57 +0700

    Mitigating SQL injection with permissions

--- src/main/java/com/manning/apisecurityinaction/Main.java
+++ src/main/java/com/manning/apisecurityinaction/Main.java
@@ -16,6 +16,9 @@ public class Main {
         "jdbc:h2:mem:natter", "natter", "password");
     var database = Database.forDataSource(datasource);
     createTables(database);
+    datasource = JdbcConnectionPool.create(
+        "jdbc:h2:mem:natter", "natter_api_user", "password");
+    database = Database.forDataSource(datasource);
 
     var spaceController = new SpaceController(database);
     post("/spaces",
