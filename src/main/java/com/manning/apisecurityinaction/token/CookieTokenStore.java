996f77811ba1d59813155f969d016edac3835034
Anh Thi Lan Nguyen <lananhnt@outlook.com.vn>
2023-11-15 22:26:24 +0700

    Double-submit cookies for the Natter API

--- src/main/java/com/manning/apisecurityinaction/token/CookieTokenStore.java
+++ src/main/java/com/manning/apisecurityinaction/token/CookieTokenStore.java
@@ -1,6 +1,9 @@
 package com.manning.apisecurityinaction.token;
 
-import java.util.Optional;
+import java.nio.charset.StandardCharsets;
+import java.security.*;
+import java.util.*;
+
 import spark.Request;
 
 public class CookieTokenStore implements TokenStore {
@@ -16,7 +19,7 @@ public class CookieTokenStore implements TokenStore {
     session.attribute("username", token.username);
     session.attribute("expiry", token.expiry);
     session.attribute("attrs", token.attributes);
-    return session.id();
+    return Base64url.encode(sha256(session.id()));
   }
 
   @Override
@@ -27,10 +30,27 @@ public class CookieTokenStore implements TokenStore {
       return Optional.empty();
     }
 
+    var provided = Base64url.decode(tokenId);
+    var computed = sha256(session.id());
+
+    if (!MessageDigest.isEqual(computed, provided)) {
+      return Optional.empty();
+    }
+
     var token = new Token(session.attribute("expiry"),
         session.attribute("username"));
     token.attributes.putAll(session.attribute("attrs"));
 
     return Optional.of(token);
+  }
+
+  static byte[] sha256(String tokenId) {
+    try {
+      var sha256 = MessageDigest.getInstance("SHA-256");
+      return sha256.digest(
+          tokenId.getBytes(StandardCharsets.UTF_8));
+    } catch (NoSuchAlgorithmException e) {
+      throw new IllegalStateException(e);
+    }
   }
 }
