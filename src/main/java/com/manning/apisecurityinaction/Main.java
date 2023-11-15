65cb78f46c382e25a47f777b48f62c7bb5e4cf75
Anh Thi Lan Nguyen <lananhnt@outlook.com.vn>
2023-11-15 21:47:09 +0700

    A token store abstraction

--- src/main/java/com/manning/apisecurityinaction/Main.java
+++ src/main/java/com/manning/apisecurityinaction/Main.java
@@ -16,6 +16,7 @@ import static spark.Spark.*;
 public class Main {
 
   public static void main(String... args) throws Exception {
+    Spark.staticFiles.location("/public");
     secure("localhost.p12", "changeit", null, null);
 
     var datasource = JdbcConnectionPool.create(
