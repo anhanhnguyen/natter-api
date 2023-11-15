9665d6ff6cd5d8422e8ea104ca7ff2f95da964e2
Anh Thi Lan Nguyen <lananhnt@outlook.com.vn>
2023-11-15 22:04:15 +0700

    Avoiding session fixation attacks

--- src/main/java/com/manning/apisecurityinaction/token/CookieTokenStore.java
+++ src/main/java/com/manning/apisecurityinaction/token/CookieTokenStore.java
@@ -8,13 +8,14 @@ public class CookieTokenStore implements TokenStore {
   @Override
   public String create(Request request, Token token) {
 
-    // WARNING: session fixation vulnerability!
-    var session = request.session(true);
-
+    var session = request.session(false);
+    if (session != null) {
+      session.invalidate();
+    }
+    session = request.session(true);
     session.attribute("username", token.username);
     session.attribute("expiry", token.expiry);
     session.attribute("attrs", token.attributes);
-
     return session.id();
   }
 
