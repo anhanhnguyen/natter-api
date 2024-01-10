e4cb3a497e662b76cb7c3b235971cc417a354d2a
Anh Thi Lan Nguyen <lananhnt@outlook.com.vn>
2024-01-10 10:46:00 +0700

    Securing the HTTPS client configuration

--- src/main/java/com/manning/apisecurityinaction/token/OAuth2TokenStore.java
+++ src/main/java/com/manning/apisecurityinaction/token/OAuth2TokenStore.java
@@ -2,14 +2,23 @@ package com.manning.apisecurityinaction.token;
 
 import org.json.JSONObject;
 import spark.Request;
+
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.*;
 import java.net.http.*;
 import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.security.GeneralSecurityException;
+import java.security.KeyStore;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.*;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLParameters;
+import javax.net.ssl.TrustManagerFactory;
+
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class OAuth2TokenStore implements SecureTokenStore {
@@ -28,7 +37,39 @@ public class OAuth2TokenStore implements SecureTokenStore {
         this.authorization = "Basic " + Base64.getEncoder()
                 .encodeToString(credentials.getBytes(UTF_8));
 
-        this.httpClient = HttpClient.newHttpClient();
+        var sslParams = new SSLParameters();
+        sslParams.setProtocols(
+                new String[] { "TLSv1.3", "TLSv1.2" });
+        sslParams.setCipherSuites(new String[] {
+                "TLS_AES_128_GCM_SHA256",
+                "TLS_AES_256_GCM_SHA384",
+                "TLS_CHACHA20_POLY1305_SHA256",
+
+                "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256",
+                "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
+                "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
+                "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
+                "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256",
+                "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256"
+        });
+        sslParams.setUseCipherSuitesOrder(true);
+        sslParams.setEndpointIdentificationAlgorithm("HTTPS");
+        try {
+            var trustedCerts = KeyStore.getInstance("PKCS12");
+            trustedCerts.load(
+                    new FileInputStream("as.example.com.ca.p12"),
+                    "changeit".toCharArray());
+            var tmf = TrustManagerFactory.getInstance("PKIX");
+            tmf.init(trustedCerts);
+            var sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, tmf.getTrustManagers(), null);
+            this.httpClient = HttpClient.newBuilder()
+                    .sslParameters(sslParams)
+                    .sslContext(sslContext)
+                    .build();
+        } catch (GeneralSecurityException | IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
@@ -54,8 +95,8 @@ public class OAuth2TokenStore implements SecureTokenStore {
 
         try {
             var httpResponse = httpClient.send(httpRequest,
-            BodyHandlers.ofString());
-            
+                    BodyHandlers.ofString());
+
             if (httpResponse.statusCode() == 200) {
                 var json = new JSONObject(httpResponse.body());
 
